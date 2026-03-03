b096e98a4c826c5f43d9b50c284dd3bf2bc520aa
Alan <alananduaga07@gmail.com>
2026-03-03 11:17:25 -0600

    Enhance authRoutes with team management endpoints
    Added routes for updating and deleting teams with admin verification.

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const { register, login, getUsers } = require('../controllers/authController');
+const { register, login, getUsers, updateTeam, deleteTeam } = require('../controllers/authController');
+const { verifyToken, isAdmin } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/users', getUsers);
+router.get('/users', getUsers); 
+
+
+router.put('/teams/:id', verifyToken, isAdmin, updateTeam);
+router.delete('/teams/:id', verifyToken, isAdmin, deleteTeam);
 
 module.exports = router;
